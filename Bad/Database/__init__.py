1e4d53d31157f59a0dcc2d98b51b8f25979d229d
hccccrr <vrajiv830@gmail.com>
2026-02-02 00:39:15 +0530

    Update __init__.py

--- Bad/Database/__init__.py
+++ Bad/Database/__init__.py
@@ -1,11 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_DB_URI
-import sys
+import config
 
-try:
-    mongodb = AsyncIOMotorClient(MONGO_DB_URI)
-    db = mongodb.PurviStringBot
-except Exception as e:
-    print(f"Error connecting to MongoDB: {e}")
-    print("Please check your MongoDB URL in config.py")
-    sys.exit(1)
+# MongoDB Connection
+mongodb = AsyncIOMotorClient(config.MONGO_DB_URI)
+db = mongodb.BadStringBot
